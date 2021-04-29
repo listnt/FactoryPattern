0fd2f40f080f1d4b10315c4cad26e067becaee04
aidar <apalon1998@mail.ru>
2021-04-29 20:58:33 +0300

    some minor changes

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,7 +4,7 @@ namespace ConsoleApp1
 {
     class Program
     {
-        
+
         static void Main(string[] args)
         {
             try
@@ -12,14 +12,16 @@ namespace ConsoleApp1
                 double[] sides = { 13, 5, 12 };
                 var someShape = ClientCode(new ThreeSideFactory(), sides);
                 double[] shapeSides = someShape.Sides;
-                //Console.WriteLine("[{0}]", string.Join(", ", shapeSides));
-                Console.WriteLine(Convert.ToString(someShape.Calculate_Area()));
+                Console.WriteLine("Sides: [{0}]", string.Join(", ", shapeSides));
+                Console.WriteLine("Calculated Area: {0}", Convert.ToString(someShape.Calculate_Area()));
+                Console.WriteLine("Type of Shape: " + someShape.GetType());
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
             }
         }
+        // Место где неизвестен тип фигуры
         public static convexShape ClientCode(IAbstractFactory someFactory, double[] sides)
         {
             var someShape = someFactory.CreateShape(sides);
